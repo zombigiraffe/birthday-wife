d44f634c6e7c6ec7b8fe63dee44ebc98d4e44195
Arron Mixell <arronmixell@yahoo.com>
2022-08-26 02:45:47 -0400

    really really finished landing page this time

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const jsConfetti = new JSConfetti();
 const confettiButton = document.getElementById("start-button");
 const theImg = document.getElementById("the-img");
 const stars = document.querySelectorAll("i");
+const nextPage = document.getElementById("next-page")
 
 function confettiThisBih() {
   jsConfetti.addConfetti();
   confettiButton.classList.add("start-button");
   confettiButton.innerHTML = "Click Me More! :)";
   theImg.classList.add("img-animation");
+  nextPage.classList.remove("hide-button");
+  nextPage.classList.add("show-button");
   let i = 0;
   function starLoop() {
     setTimeout(function () {
